81ee247266c8b07f2040cb3e284c95651fda442b
Savvynandha <savvynandha@gmail.com>
2017-09-25 07:39:12 -0700

    Add files via upload

--- BarcodeIO.java
+++ BarcodeIO.java
@@ -1,13 +1,15 @@
-/**
- * 
- */
 
-
-public interface BarcodeIO {
+public interface BarcodeIO    //It only contains abstract methods to be inherited and implemented by DataMatrix class
+{
 	public boolean scan(BarcodeImage bc);
+	
 	public boolean readText(String text);
+	
 	public boolean generateImageFromText();
+	
 	public boolean translateImageToText();
+	
 	public void displayTextToConsole();
-	public void displayImageToConsole();
+	
+	void displayImageToConsole();
 }
